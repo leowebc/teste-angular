8dec27dbfcbd14e33e41fc6cd6ea584b0a1da5b4
leowebc <leowebc@gmail.com.br>
2025-01-22 11:33:10 -0300

    feat(TasksService) utiliza HttpClient para fazer requisições

--- src/app/sherad/services/tasks/tasks.service.ts
+++ src/app/sherad/services/tasks/tasks.service.ts
@@ -1,24 +1,17 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from '../../interfaces/task.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
+
+  httpClient = inject(HttpClient);
+
   getAll(): Observable<Task[]> {
-    return of([ 
-      { title: 'Item 1', completed: false },
-      { title: 'Item 2', completed: false },
-      { title: 'Item 3', completed: false },
-      { title: 'Item 4', completed: false },
-      { title: 'Item 5', completed: false },
-      { title: 'Item 6', completed: true },
-      { title: 'Item 7', completed: true },
-      { title: 'Item 8', completed: true },
-      { title: 'Item 9', completed: true },
-      { title: 'Item 10', completed:true },
-    ]);
-  }
+    return this.httpClient.get<Task[]>('/tasks');
+  } 
 }
