937d138d44e53456998a63ec7ad75cc8a741e452
leowebc <leowebc@gmail.com.br>
2025-01-22 03:53:24 -0300

    feat(ListComponent): implementa listagem de forma dinâmica usando DaisyUI e Tailwind

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+interface Task{
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-list',
@@ -8,4 +13,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+
+ tasks = signal<Task[]>([
+  { title: 'Item 1', completed: false },
+  { title: 'Item 2', completed: false },
+  { title: 'Item 3', completed: false },
+  { title: 'Item 4', completed: false },
+  { title: 'Item 5', completed: false },
+  { title: 'Item 6', completed: true },
+  { title: 'Item 7', completed: true },
+  { title: 'Item 8', completed: true },
+  { title: 'Item 9', completed: true },
+  { title: 'Item 10', completed:true },
+])
+
+  completedTasks = computed(() => 
+    this.tasks().filter(task => task.completed)
+  )
+
+  pendingTasks = computed(() => 
+    this.tasks().filter(task => !task.completed)
+  )
+
+}
