681c5a767c829d102276ae9a66f3e7db39859caa
leowebc <leowebc@gmail.com.br>
2025-01-22 07:04:34 -0300

    feat(TaskInterface): cria serviço

--- src/app/sherad/services/tasks/tasks.service.ts
+++ src/app/sherad/services/tasks/tasks.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Task } from '../../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  getAll(){
-    return of([]);
+  getAll(): Observable<Task[]> {
+    return of([ 
+      { title: 'Item 1', completed: false },
+      { title: 'Item 2', completed: false },
+      { title: 'Item 3', completed: false },
+      { title: 'Item 4', completed: false },
+      { title: 'Item 5', completed: false },
+      { title: 'Item 6', completed: true },
+      { title: 'Item 7', completed: true },
+      { title: 'Item 8', completed: true },
+      { title: 'Item 9', completed: true },
+      { title: 'Item 10', completed:true },
+    ]);
   }
 }
