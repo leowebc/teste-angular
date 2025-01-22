6d09d99883ea4eb493c27be4584eea9ab57bba22
leowebc <leowebc@gmail.com.br>
2025-01-22 14:26:53 -0300

    feat(NoItemsComponent): cria componente

---
+++ src/app/pages/list/no-items/no-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-items.component.html',
+  styleUrl: './no-items.component.scss'
+})
+export class NoItemsComponent {
+
+}
