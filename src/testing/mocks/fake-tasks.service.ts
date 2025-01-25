97a6fc9bc996d3a857db70b18efca4061128108b
leowebc <leowebc@gmail.com.br>
2025-01-25 12:15:24 -0300

    corrigindo arquivos

--- src/testing/mocks/fake-tasks.service.ts
+++ src/testing/mocks/fake-tasks.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { TasksService } from 'src/app/sherad/services/tasks/tasks.service';                              
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';                              
 export class FakeTasksService implements TasksService {
   httpClient!: HttpClient;
   getAll = jest.fn();
